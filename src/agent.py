8b732c495ac1fdfae90b4eecc7504adbac53f207
Hugo Sobral <hugosobral10@gmail.com>
2023-01-26 16:23:04 -0300

    Add agent example
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>

---
+++ src/agent.py
@@ -0,0 +1,25 @@
+from mesa import Agent, Model
+
+class CellAgent(Agent):
+    def __init__(self, unique_id: int, model: Model) -> None:
+        super().__init__(unique_id, model)
+        self.money = 2
+
+    def give_money(self) -> None:
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other = self.random.choice(cellmates)
+            other.money += 1
+            self.money -= 1
+
+    def move(self) -> None:
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def step(self) -> None:
+        self.move()
+        if self.money > 0:
+            self.give_money()
