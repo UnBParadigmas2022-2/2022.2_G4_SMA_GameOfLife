2e0175072639399338adbeb2a7bab24f26ea142e
Hugo Sobral <hugosobral10@gmail.com>
2023-01-26 16:27:20 -0300

    Add server base structure
    Co-authored-by: Micaella Gouveia <micaella2212@gmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>

---
+++ src/server.py
@@ -0,0 +1,53 @@
+from agent import CellAgent
+from model import CellModel
+
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
+
+SIZE = 50
+X_SIZE = 800
+Y_SIZE = 800
+
+
+def agent_portrayal(agent: CellAgent):
+    portrayal = {
+        "Shape": "circle",
+        "Filled": "true",
+    }
+
+    if agent.money >= 3:
+        portrayal["Color"] = "blue"
+        portrayal["Layer"] = 0
+        portrayal["r"] = 0.8
+    
+    elif agent.money > 0:
+        portrayal["Color"] = "green"
+        portrayal["Layer"] = 1
+        portrayal["r"] = 0.5
+    
+    else:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 2
+        portrayal["r"] = 0.2
+    
+    return portrayal
+
+simulation_params = {
+    "num_agents": UserSettableParameter(
+        "slider",
+        name="Número de agentes da simulação",
+        value=40,
+        min_value=20,
+        max_value=150,
+        step=1,
+        description="Slider para definir a quantidade de agentes a serem simulados"
+    ),
+    "width": SIZE,
+    "height": SIZE
+}
+
+grid_visualization = CanvasGrid(agent_portrayal, SIZE, SIZE, X_SIZE, Y_SIZE)
+server = ModularServer(CellModel, [grid_visualization], "Game of Life", simulation_params)
+server.port = 8521
+server.launch()
