9c259995025f18d715f41c6884d5fb243fc6a024
Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>
2023-01-28 20:15:30 -0300

    Filtro de vizinhos vivos e mortos e o renascimento

--- src/GameOfLife/agent.py
+++ src/GameOfLife/agent.py
@@ -1,6 +1,5 @@
 from mesa import Agent, Model
 
-
 class CellAgent(Agent):
     def __init__(self, unique_id: int, model: Model, alive: bool) -> None:
         super().__init__(unique_id, model)
@@ -13,13 +12,44 @@ class CellAgent(Agent):
         print(len(neighbors))
         print(neighbors[0])
         # TODO: realizar filtro de vizinhos vivos
-        # TODO: realizar filtro de vizinhos mortos
-        # TODO: Acionar método de renaissance ou death de acordo com as regras do jogo
+        def count_neighbors(self, grid):
+            neighbors = 0
+            for i in range(self.x-1, self.x+2):
+                for j in range(self.y-1, self.y+2):
+                    if (i, j) != (self.x, self.y) and 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+                        if grid[i][j].state == 1:
+                            neighbors += 1
+            return neighbors
 
-    def renaissance(self) -> None:
+        # TODO: realizar filtro de vizinhos mortos
+        def count_dead_neighbors(self, grid):
+            dead_neighbors = 0
+            for i in range(self.x-1, self.x+2):
+                for j in range(self.y-1, self.y+2):
+                    if (i, j) != (self.x, self.y) and 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+                        if grid[i][j].state == 0:
+                            dead_neighbors += 1
+            return dead_neighbors
+        # TODO: Acionar método de renaissance ou death de acordo com as regras do jogo
+        def update_cell(self, x, y):
+            cell = self.grid[x][y]
+            live_neighbors = cell.count_neighbors(self.grid)
+            dead_neighbors = cell.count_dead_neighbors(self.grid)
+            if cell.state == 1:
+                if live_neighbors < 2 or live_neighbors > 3:
+                    cell.death()
+                if live_neighbors >= 3:
+                    cell.death()
+                if live_neighbors < 2:
+                    cell.death()
+            else:
+                if dead_neighbors == 3:
+                    cell.renaissance()
+
+    def renaissance(self) -> None: #revive
         self.alive = True
 
-    def death(self) -> None:
+    def death(self) -> None: #morre
         self.alive = False
 
     def step(self) -> None:
