d24f5be9a9e7010a99f5fc4dd31067ab0ec75ffa
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2023-01-30 21:16:16 -0300

    create env vars
    co-authored-by: enzoggqs <eggqsaraiva@gmail.com>

--- src/server.py
+++ src/server.py
@@ -1,11 +1,12 @@
 from src.FinancialBehavior.server import financial_server
 from src.GameOfLife.server import game_of_life_server
+import os
 
-financial_behavior = False # Vai ser uma env var
+financial_behavior = os.environ.get("FINANCIAL_BEHAVIOR", False)
 
 server = None
 
-if financial_behavior:
+if financial_behavior == "true":
     server = financial_server
 else:
     server = game_of_life_server
